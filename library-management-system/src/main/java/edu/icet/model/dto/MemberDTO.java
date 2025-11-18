a2b1798147ff432f47c36b764c7cf6570fe54e48
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-11-18 14:24:27 +0530

    Add Member DTO class

---
+++ library-management-system/src/main/java/edu/icet/model/dto/MemberDTO.java
@@ -0,0 +1,12 @@
+package edu.icet.model.dto;
+
+public class MemberDTO {
+
+    private String memberId;
+    private String name;
+    private String nic;
+    private String phone;
+    private String email;
+    private String address;
+
+}
